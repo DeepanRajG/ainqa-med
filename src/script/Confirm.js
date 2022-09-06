4d9f0444a7c1d46d0c2db7a0b4f78cdc629c550a
sowmiyaashok <109061556+sowmiyaashok@users.noreply.github.com>
2022-09-06 16:36:40 +0530

    confirm header

--- src/script/Confirm.js
+++ src/script/Confirm.js
@@ -1,14 +1,61 @@
 import '../css/confirm.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useNavigate } from "react-router-dom";
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 function App(){
+  const navigate = useNavigate();
+    const func = () => {
+        var e = document.getElementById("selectopt");
+        
+        var value = e.value;
+        var text = e.options[e.selectedIndex].text;
+        console.log(text)
+        if(text=="Log Out"){
+            let path = '/login';
+            navigate(path);
+        }
+    }
 return(
     <div style={{ display: 'block', }}>
-      <header>
-        <h4 id="head">QUICK  REGISTRATION</h4>
-        <h4 id="con">CONFIRMATION</h4>
-     </header>
+      <header id="header0">
+                <Row>
+                    <Col> <p id="quick"> QUICK REGISTRATION</p></Col>
+
+
+                    <div id="wether"></div>
+                    <p id="p1">30 clody</p>
+                    <div id='line'></div>
+                    <div id="settings"></div>
+                    <div id="join"></div>
+                    <div id="noti"></div>
+                    <form id="para">
+                    <select  id='selectopt'  onChange={func} value='' >
+                  
+                    <option  hidden >Jennifer Jo</option>
+                        <option value='profile'>Profile</option>
+                        <option value='settings' >Settings</option>
+                        <option value='logout'  >Log Out</option>
+                        
+                    </select>
+                    <p id="position">Paramedic</p>
+                    </form>
+                    <div id='line'></div>
+                    <div id="profile" ></div>
+
+                    
+   
+
+
+
+
+
+
+
+                </Row>
+
+
+            </header>
      <Row id="main" style={{
         paddingTop: '30px',
         paddingBottom: '30px',
